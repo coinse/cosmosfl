dc0b8ee61315ef754718c4078b3ecef360c40976
HyunjoonCho <jhj09016@gmail.com>
2026-02-05 20:50:48 -0800

    add results for gemma and phi3 / force selection scenarios

--- autofl/constrained_exp.sh
+++ autofl/constrained_exp.sh
@@ -19,17 +19,17 @@ sh constrained_runner.sh constraints Qwen/Qwen2.5-Coder-7B-Instruct defects4j gu
 sh constrained_runner.sh force_select Qwen/Qwen2.5-Coder-7B-Instruct defects4j guidance false true 
 sh constrained_runner.sh without_constraints Qwen/Qwen2.5-Coder-7B-Instruct defects4j hf false false
 
-# sh constrained_runner.sh constraints_with_no_repetitions mistralai/Mistral-Nemo-Instruct-2407 defects4j guidance true false
-# sh constrained_runner.sh no_repeat_force_select mistralai/Mistral-Nemo-Instruct-2407 defects4j guidance true true
-# sh constrained_runner.sh constraints mistralai/Mistral-Nemo-Instruct-2407 defects4j guidance false false
-# sh constrained_runner.sh force_select mistralai/Mistral-Nemo-Instruct-2407 defects4j guidance false true 
-# sh constrained_runner.sh without_constraints mistralai/Mistral-Nemo-Instruct-2407 defects4j hf false false
- 
-# sh constrained_runner.sh constraints_with_no_repetitions microsoft/phi-4 defects4j guidance true false
-# sh constrained_runner.sh no_repeat_force_select microsoft/phi-4 defects4j guidance true true
-# sh constrained_runner.sh constraints microsoft/phi-4 defects4j guidance false false
-# sh constrained_runner.sh force_select microsoft/phi-4 defects4j guidance false true 
-# sh constrained_runner.sh without_constraints microsoft/phi-4 defects4j hf false false
+sh constrained_runner.sh constraints_with_no_repetitions google/gemma-2-9b-it defects4j guidance true false
+sh constrained_runner.sh no_repeat_force_select google/gemma-2-9b-it defects4j guidance true true
+sh constrained_runner.sh constraints google/gemma-2-9b-it defects4j guidance false false
+sh constrained_runner.sh force_select google/gemma-2-9b-it defects4j guidance false true 
+sh constrained_runner.sh without_constraints google/gemma-2-9b-it defects4j hf false false
+
+sh constrained_runner.sh constraints_with_no_repetitions microsoft/Phi-3-mini-4k-instruct defects4j guidance true false
+sh constrained_runner.sh no_repeat_force_select microsoft/Phi-3-mini-4k-instruct defects4j guidance true true
+sh constrained_runner.sh constraints microsoft/Phi-3-mini-4k-instruct defects4j guidance false false
+sh constrained_runner.sh force_select microsoft/Phi-3-mini-4k-instruct defects4j guidance false true 
+sh constrained_runner.sh without_constraints microsoft/Phi-3-mini-4k-instruct defects4j hf false false
 
 sh constrained_runner.sh constraints_with_no_repetitions meta-llama/Llama-3.2-3B-Instruct defects4j guidance true false
 sh constrained_runner.sh no_repeat_force_select meta-llama/Llama-3.2-3B-Instruct defects4j guidance true true
